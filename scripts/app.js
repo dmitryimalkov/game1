f973ddf94ddf792878e6b907966ace851d70df7a
dmitryimalkov <dmitry.i.malkov@gmail.com>
2024-02-19 12:19:30 +0300

    all

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 //initiate variables
+//Overlay
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form'); //get access to form element
+
+//Buttons
+const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+
+//Button Listeners
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig); //listening to submit event
--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,18 @@
 // open-close forms and overlays
+function openPlayerConfig() {
+  // get access to and overlay element and change css property display from none
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
+//in the next function, first of all, we have to prevent browser for sending an input data to the server
+// this is done by event
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername");
+  console.log(enteredPlayername);
+}
